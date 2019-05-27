c7691912958020e95d9e0aa0d4ab0004f8235131
shrouski <shrouski@gmail.com>
2019-05-27 11:53:06 +0400

    Add qrencode, Change IP, Remove Hosts/tar

--- add-client.sh
+++ add-client.sh
@@ -8,16 +8,12 @@ else
 	mkdir -p clients/$1
 	wg genkey | tee clients/$1/$1.priv | wg pubkey > clients/$1/$1.pub
 	key=$(cat clients/$1/$1.priv) 
-	ip="10.8.0."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
+	ip="10.201.3."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
 	FQDN=$(hostname -f)
-	cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_ADDRESS:|'"$FQDN"'|' > clients/$1/wg0.conf
+	cat wg0-client-template.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_ADDRESS:|'"$FQDN"'|' > clients/$1/wg0.conf
 	echo $ip > last-ip.txt
-	cp SETUP.txt clients/$1/SETUP.txt
-	tar czvf clients/$1.tar.gz clients/$1
-	echo "Created config!"
+	echo "Created client config!"
 	echo "Adding peer"
 	sudo wg set wg0 peer $(cat clients/$1/$1.pub) allowed-ips $ip/32
-	echo "Adding peer to hosts file"
-	echo $ip" "$1 | sudo tee -a /etc/hosts
-	sudo wg show
+	qrencode -t ansiutf8 < cl1.conf
 fi
